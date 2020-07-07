60931390870eaf6dae6c9a4dcdd614812ec395aa
Maksim Maltcev <maximus3506@mail.ru>
2020-07-07 21:15:20 +0300

    lesson 19 hard done

--- js/script.js
+++ js/script.js
@@ -77,6 +77,39 @@ const toggleMenu = ()  => {
             menu.classList.remove('active-menu');
         }
     });
+
+    const scrolling = () => {
+        //плавная прокрутки при нажатии на меню
+        menu.addEventListener('click', event => {
+            const target = event.target;
+
+            if (target.closest('menu ul>li>a')) {//выбираем ссылки
+                event.preventDefault();//отменим стандартное поведение
+                const link = target.closest('menu ul>li>a').getAttribute('href');//получаем атрибут ссылку
+                document.querySelector(link).scrollIntoView({//на полученной ссылке применяем метод scroll
+                    block: 'start',
+                    behavior: 'smooth'
+                });
+            }
+        });
+        // Плавная прокрутка при нажатии на стрелку
+        const main = document.querySelector('main');//получим main
+        main.addEventListener('click', event => {
+            const target = event.target;
+
+            if (target.closest('a>img')) { //выбираем сслыку-картинку
+                event.preventDefault();
+                //по аналогии с со ссылками
+                const link = target.closest('a').getAttribute('href');
+                document.querySelector(link).scrollIntoView({
+                    block: 'start',
+                    behavior: 'smooth'
+                });
+            }
+        });
+    };
+    scrolling();
+
 };
 
 toggleMenu();
